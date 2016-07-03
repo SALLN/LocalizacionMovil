38d628d5235cac5ccd7c8fc584ce31a82813309c
SALLN <stevenllerena@hotmail.com>
2016-07-02 21:32:10 -0500

    Funciona perfecta

--- app/src/main/java/prueba/app/llerena/steven/com/localizacionmovil/MainActivity.java
+++ app/src/main/java/prueba/app/llerena/steven/com/localizacionmovil/MainActivity.java
@@ -27,6 +27,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -34,13 +35,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 import java.io.BufferedOutputStream;
 import java.io.Console;
@@ -60,9 +66,9 @@ public class MainActivity extends AppCompatActivity {
     MyLocationListenerRed LocListenerRed;
     LocationManager LocManagerRed;
     LocationManager LocManagerGps;
-    Button boton,aunten;
-    EditText edit1,edit2,edit3,editurl,nombre,contra;
-    TextView estado,inc;
+    Button boton_activar,aunten;
+    EditText edit_tiempo,edit_distancia,edit_idvehiculo,edit_url,nombre,contra;
+    TextView text_estado,inc;
     String Posiciones[][] = new String[500][5];
     String proveedor = null;
     String fecha_gps= null ;
@@ -90,6 +96,11 @@ public class MainActivity extends AppCompatActivity {
     IntentFilter filter;
     Intent batteryStatus;
     NotificationManager notifyMgr;
+    ViewFlipper vf;
+    boolean Inicio_Sesion=false;
+    float init_x;
+    int duracion=300;
+    int Vista=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,72 +116,139 @@ public class MainActivity extends AppCompatActivity {
         int p = android.os.Process.myPid();
         android.os.Process.killProcess(p);        }
 
+        setContentView(R.layout.flipper);
 
-        aunten = (Button) findViewById(R.id.entrar_boton);
-        inc = (TextView) findViewById(R.id.incorrecto);
-
-        nombre = (EditText) findViewById(R.id.nombre_input);
-        contra = (EditText) findViewById(R.id.contrasena_input);
-
-        nombre.setText(prefs.getString("Usuario", ""));
-        contra.setText(prefs.getString("Contra", ""));
+        Inicializar();
         Notificacion();
 
-        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        registerReceiver(battery_receiver, filter);
+        //IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        //registerReceiver(battery_receiver, filter);
+
+        //handler = new Handler();
+        //handler.postDelayed(updateData2,500);
+
+        vf = (ViewFlipper) findViewById(R.id.viewFlipper);
+        vf.setOnTouchListener(new ListenerTouchViewFlipper());
+    }
+
+    private class ListenerTouchViewFlipper implements View.OnTouchListener{
+
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN: //Cuando el usuario toca la pantalla por primera vez
+                    init_x=event.getX();
+                    return true;
+                case MotionEvent.ACTION_UP: //Cuando el usuario deja de presionar
+                    float distance =init_x-event.getX();
+
+                    if(distance<0)
+                    {
+                        vf.setInAnimation(inFromLeftAnimation());
+                        vf.setOutAnimation(outToRightAnimation());
+                        vf.showPrevious();
+                        if (Vista==3 || Vista==2){     Vista--;   }else{    Vista=3;    }
+                    }
+
+                    if(distance>0)
+                    {
+                        vf.setInAnimation(inFromRightAnimation());
+                        vf.setOutAnimation(outToLeftAnimation());
+
+                        vf.showNext();
+                        if (Vista==1 || Vista==2){     Vista++;   }else{    Vista=1;    }
+                    }
+
+                default:
+                    break;
+            }
+            return false;
+        }
+    }
+
+    private Animation inFromRightAnimation() {
+
+        Animation inFromRight = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT,  +1.0f, Animation.RELATIVE_TO_PARENT,  0.0f,
+                Animation.RELATIVE_TO_PARENT,  0.0f, Animation.RELATIVE_TO_PARENT,   0.0f );
+
+        inFromRight.setDuration(duracion);
+        inFromRight.setInterpolator(new AccelerateInterpolator());
+
+        return inFromRight;
+
+    }
+
+    private Animation outToLeftAnimation() {
+        Animation outtoLeft = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, -1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        outtoLeft.setDuration(duracion);
+        outtoLeft.setInterpolator(new AccelerateInterpolator());
+        return outtoLeft;
+    }
+
+    private Animation inFromLeftAnimation() {
+        Animation inFromLeft = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, -1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        inFromLeft.setDuration(duracion);
+        inFromLeft.setInterpolator(new AccelerateInterpolator());
+        return inFromLeft;
+    }
+
+    private Animation outToRightAnimation() {
+        Animation outtoRight = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, +1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        outtoRight.setDuration(duracion);
+        outtoRight.setInterpolator(new AccelerateInterpolator());
+        return outtoRight;
     }
 
     public void BotonEntrar(View v2) {
 
-        CheckBox checkk = (CheckBox) findViewById(R.id.recordar_check);
-        boolean regis = checkk.isChecked();
-        Usuario=nombre.getText().toString();
-        Contrasena=contra.getText().toString();
+        if(!Inicio_Sesion) {
+            CheckBox checkk = (CheckBox) findViewById(R.id.recordar_check);
+            boolean regis = checkk.isChecked();
+            Usuario = nombre.getText().toString();
+            Contrasena = contra.getText().toString();
 
-        if(regis) {
-            editor.putString("Usuario", Usuario);
-            editor.putString("Contra", Contrasena);
-            editor.commit();
+            if (regis) {
+                editor.putString("Usuario", Usuario);
+                editor.putString("Contra", Contrasena);
+                editor.commit();
+            }
+            aunten.setText("INICIANDO SESION...");
+
+            IniciarSesion iniciarSesion = new IniciarSesion();
+            iniciarSesion.execute();
+        }else{
+            aunten.setText("INICIAR SESION");
+            DesactivarSistema(findViewById(R.id.id_vehiculo));
         }
-        aunten.setText("AUTENTICANDO...");
-
-        IniciarSesion iniciarSesion = new IniciarSesion();
-        iniciarSesion.execute();
 }
-
-    public void Autenticado(){
-        SistemaGeo();
-    }
-
-    public void SistemaGeo(){
-
-        setContentView(R.layout.activity_main);
-        boton = (Button) findViewById(R.id.botonactivar);
-        edit1= (EditText) findViewById(R.id.tiempo);
-        edit2= (EditText) findViewById(R.id.distancia);
-        edit3= (EditText) findViewById(R.id.id_vehiculo);
-        editurl = (EditText) findViewById(R.id.editurl);
-        edit3.setText(prefs.getString("ID", ""));
-
-        estado = (TextView) findViewById(R.id.estado);
-
-        LocManagerGps = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        LocManagerRed = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        LocListenerGps = new MyLocationListenerGps();
-        LocListenerRed = new MyLocationListenerRed();
-    }
 
     public void ActivarSistema(View v) {
 
-        estado.setText(R.string.activar);
-        estado.setBackgroundColor(Color.parseColor("#0bf43d"));
 
-        tiempo_espera = Integer.parseInt(edit1.getText().toString())*1000 ;
-        metros_espera = Integer.parseInt(edit2.getText().toString());
+        text_estado.setText(R.string.activar);
+        text_estado.setBackgroundColor(Color.parseColor("#0bf43d"));
 
-        URLL = editurl.getText().toString();
-        ID_VEHICULO = edit3.getText().toString();
-        edit1.setActivated(false);
+        tiempo_espera = Integer.parseInt(edit_tiempo.getText().toString())*1000 ;
+        metros_espera = Integer.parseInt(edit_distancia.getText().toString());
+
+        URLL = edit_url.getText().toString();
+        ID_VEHICULO = edit_idvehiculo.getText().toString();
+        edit_tiempo.setActivated(false);
 
         editor.putString("ID",ID_VEHICULO);
         editor.putString("URL",URLL);
@@ -187,8 +265,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void DesactivarSistema(View vi){
 
-        estado.setText(R.string.desactivar);
-        estado.setBackgroundColor(Color.parseColor("#f40b49"));
+
+        text_estado.setText(R.string.desactivar);
+        text_estado.setBackgroundColor(Color.parseColor("#f40b49"));
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED         && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)         {return;        }
 
@@ -346,7 +425,9 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {super.onPostExecute(result);
             if (respuestaservidor.equals("OK")){
-                Autenticado();
+                Inicio_Sesion=true;
+                aunten.setText("CERRAR SESION");
+                aunten.setBackgroundColor(Color.parseColor("#0bf43d"));
             }else{
                 inc.setText("Usuario o contraseña Incorrectos");
                 aunten.setText("AUTENTICAR");
@@ -434,8 +515,33 @@ public class MainActivity extends AppCompatActivity {
 
     public void Cerrar(){        finish();    }
 
-    public void texto_ombe(){    edit3.setText(textbateria);    }
+    public void Inicializar(){
 
+        aunten = (Button) findViewById(R.id.entrar_boton);
+        inc = (TextView) findViewById(R.id.incorrecto);
+
+        nombre = (EditText) findViewById(R.id.nombre_input);
+        contra = (EditText) findViewById(R.id.contrasena_input);
+
+        nombre.setText(prefs.getString("Usuario", ""));
+        contra.setText(prefs.getString("Contra", ""));
+
+        boton_activar = (Button) findViewById(R.id.botonactivar);
+        edit_tiempo= (EditText) findViewById(R.id.tiempo);
+        edit_distancia= (EditText) findViewById(R.id.distancia);
+        edit_idvehiculo= (EditText) findViewById(R.id.id_vehiculo);
+        edit_url = (EditText) findViewById(R.id.editurl);
+        edit_idvehiculo.setText(prefs.getString("ID", "Steven"));
+
+        text_estado = (TextView) findViewById(R.id.estado);
+
+        LocManagerGps = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocManagerRed = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocListenerGps = new MyLocationListenerGps();
+        LocListenerRed = new MyLocationListenerRed();
+    }
+
+    public void texto_ombe(){    edit_idvehiculo.setText(textbateria);    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -450,46 +556,72 @@ public class MainActivity extends AppCompatActivity {
         switch ( item.getItemId() )
         {
             case R.id.mapa:
-            {
-                Toast.makeText(this, "Botón mapa pulsado", Toast.LENGTH_SHORT).show();
-            }
+                if (Vista==1){
+                    vf.setInAnimation(inFromLeftAnimation());
+                    vf.setOutAnimation(outToRightAnimation());
+                    vf.showPrevious();
+                }else if(Vista==2){
+                    vf.setInAnimation(inFromRightAnimation());
+                    vf.setOutAnimation(outToLeftAnimation());
+                    vf.showNext();
+                }
+                Vista=3;
             break;
 
             case R.id.signin:
-            {
-                Intent intento2 = new Intent(MainActivity.this,Signin.class);
-                startActivity(intento2);
-            }
+                if (Vista==3){
+                    vf.setInAnimation(inFromRightAnimation());
+                    vf.setOutAnimation(outToLeftAnimation());
+                    vf.showNext();
+                }else if(Vista==2){
+                    vf.setInAnimation(inFromLeftAnimation());
+                    vf.setOutAnimation(outToRightAnimation());
+                    vf.showPrevious();
+                }
+                Vista=1;
             break;
 
             case R.id.config:
-            {
-                setContentView(R.layout.activity_main);
-
-            }
+                if (Vista==1){
+                    vf.setInAnimation(inFromRightAnimation());
+                    vf.setOutAnimation(outToLeftAnimation());
+                    vf.showNext();
+                }else if(Vista==3){
+                    vf.setInAnimation(inFromLeftAnimation());
+                    vf.setOutAnimation(outToRightAnimation());
+                    vf.showPrevious();
+                }
+                Vista=2;
             break;
 
             default:
                 Toast.makeText(this, "Botón config pulsado", Toast.LENGTH_SHORT).show();
-
                 return super.onOptionsItemSelected(item);
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    private Runnable updateData2 = new Runnable(){
+
+        public void run(){
+            handler.postDelayed(updateData2,1000);
+        }
+    };
+
+
+
     private BroadcastReceiver battery_receiver = new BroadcastReceiver() {
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-    int plugged = intent.getIntExtra("plugged", -1);
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int plugged = intent.getIntExtra("plugged", -1);
             if (plugged==1){
 
                // Toast.makeText(getBaseContext(),"ENCHUFADO CON AC",Toast.LENGTH_SHORT).show();
             }else {
                 //Toast.makeText(getBaseContext(),"ENCHUFADO CON DC O NO ENCHUFADO",Toast.LENGTH_SHORT).show();
             }
-
             int rawlevel = intent.getIntExtra("level", -1);
             int level = 0;
 
